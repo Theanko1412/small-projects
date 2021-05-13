9e0c648f5c4443d28d30c4761d5f6f853d8c5426
Theanko <danko.curlin@hotmail.com>
2021-05-13 18:41:12 +0200

    Update 2.0
    No longer has bug with multiple monitors, runs without user inputs...

--- TwitchRewardAutoClicker/README.md
+++ TwitchRewardAutoClicker/README.md
@@ -1,17 +1,15 @@
-## TwitchRewardAutoClicker 
+# TwitchRewardAutoClicker 2.0
 
-Autoclicking program that clicks the reward button on twitch.tv for you.
+This is simple autoclicking program that collects the rewards on twitch.tv for you, it automaticaly detects where the reward is on the screen and collects it within 4 seconds(can be modified for speed or system usage). 
 
-When starting the program press number 1 if you want to use standard coordinates of the button(1080p resolution). If you want to put custom ones press 2.
+## 2.0 Update
 
-### Custom coordinates
-If you pressed option 2 into the terminal your are choosing custom coordinates. To make them put the cursor(Do not click) on top left side of the reward button and apply it with enter key, do the same thing with bottom right corner. If you do not have reward at the time of starting the program just wait a bit. (new reward is comming every 15ish minutes)
- 
-### Stoping the program
-Program is stopped with holding the escape key(could take up to 30 seconds because of the sleep function to reduce system usage (can be stopped with 'x' button too)
+* The new version no longer has bug with more than one monitor 
+* No longer needs user input 
+* Works with every monitor resolution
+* Detects the button on the whole screen(and more than one!), not only selected area
 
-To install and start using it you can dowload the zip folder, unpack it and find the .exe file.  --author Danko Čurlin
+## Instalation
 
-
-
-*the program currently does not work if the button is located on the second monitor*
+To install and start using it download the zip folder, unpack it and find the .exe file. (or run the .py file)
+> --author Danko Čurlin
--- TwitchRewardAutoClicker/twitchautoclicker.py
+++ TwitchRewardAutoClicker/twitchautoclicker.py
@@ -1,108 +1,92 @@
 import datetime
+from PIL.ImageOps import grayscale
 import pyautogui
 import time
 import keyboard
 import random
 import win32api, win32con
+import sys
+from PIL import ImageGrab
+from functools import partial
+
+ImageGrab.grab = partial(ImageGrab.grab, all_screens=True)
+
+""" The program is autoclicking the reward button on twitch.tv using the win32api + pyautogui. Program is stopped with holding 
+the escape key(could take up to 5 seconds because of the sleep function to reduce system usage(can be modified)), 
+or it can be killed normally in terminal.  --author Danko Čurlin"""
 
 
-""" The program is autoclicking the reward button on twitch.tv, upon starting the program when pressed
-number 1 it will use standard coordinates of a button(1080p resolution), if needed to put custom ones
-press q, put the cursor on top left side of the button and apply it with enter key, do the same thing with 
-bottom right corner.  Program is stopped with holding the escape key(could take up to 30 seconds because of
-sleep function to reduce system usage, can be stopped with 'x' button too.  -- @author Danko Čurlin"""
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = "{:02d}".format(secs)
+        print("Starting in ", timer, end="\r")
+        time.sleep(1)
+        t -= 1
 
 
-#defining standard position of the button
-global testvar
-testvar = 'q'
-global topleftX
-topleftX = 1669
-global bottomrightX
-bottomrightX = 1683
-global topleftY
-topleftY = 1004
-global bottomrightY
-bottomrightY = 1025
-
-def countdown(t): 
-	while t: 
-		mins, secs = divmod(t, 60) 
-		timer = '{:02d}'.format(secs) 
-		print("Starting in ", timer, end="\r") 
-		time.sleep(1) 
-		t -= 1
-
-#bot actions
+# bot actions
 def botStart():
     start_time = time.time()
     collected = 0
     StreamerGone = 0
-    while keyboard.is_pressed('esc') == False:
-        if pyautogui.locateOnScreen('rewardButton.png') != None:
+    while keyboard.is_pressed("esc") == False:
+        if pyautogui.locateOnScreen("rewardButton.png", grayscale=True) != None:
+            try:
+                coordinates = pyautogui.locateOnScreen(
+                    "rewardButton.png", grayscale=True
+                )
+            except TypeError:
+                time.sleep(2)
+                continue
+            # randomising key pressing to avoid possible bot detection
+            try:
+                x = random.randint(
+                    int(coordinates[0]), int(coordinates[0] + coordinates[2])
+                )
+                y = random.randint(
+                    int(coordinates[1]), int(coordinates[1] + coordinates[3])
+                )
+            except TypeError:
+                time.sleep(2)
+                continue
+            click(x, y)
+            time.sleep(0.5)
+            click(x, y - 45)
+            time.sleep(4)
             StreamerGone = 0
             collected += 50
             print("Collected points >> ", collected)
-            print("Running time     >> " + str((time.time() - start_time)/60) + " minutes")
+            print(
+                "Running time     >> "
+                + str((time.time() - start_time) / 60)
+                + " minutes"
+            )
+            print(
+                "Current average  >> ",
+                str(collected / ((time.time() - start_time) / 60)) + " ppm",
+            )
             print("#############################################################")
-            print("Current average  >> ", str(collected / ((time.time() - start_time) / 60)) + " ppm")
-            print("#############################################################")
-            #randomising key pressing
-            clickyclick(random.randint(topleftX, bottomrightX), random.randint(topleftY, bottomrightY))
-            time.sleep(0.5)
-            moveymove()
-            time.sleep(5)
         else:
-            StreamerGone += 1
-            #ending if passed one hour without a reward
-            if (StreamerGone == 120):
+            StreamerGone += 4
+            # ending if passed one hour without a reward
+            if StreamerGone == 3600:
                 break
             else:
-                time.sleep(30)
+                time.sleep(4)
 
-#clicking
-def clickyclick(x, y):
-    win32api.SetCursorPos((x,y))
+
+# clicking
+def click(x, y):
+    win32api.SetCursorPos((x, y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
 
-#reseting the mouse position because if its left hovering on button, pyautogui can not find the button
-def moveymove():
-    win32api.SetCursorPos((1000, 500))
 
-
-
-#type functions
-def goStandard():
+def start():
     countdown(3)
     print("\nRunning...")
     botStart()
 
-def goCustom():
-    print("Define a rectangle with hovering over edges of a square and pressing on letter 'q', try to be as close as possible")
-    testinput = input("Click on top left edge of the square >> ")
-    if(testinput == testvar):
-        topleftX, topleftY = pyautogui.position()
-        print(topleftX, topleftY)
-        testinput = None
 
-    testinput = input("Click on bottom right edge of the square >> ")
-    if(testinput == testvar):
-        bottomrightX, bottomrightY = pyautogui.position()
-        print(bottomrightX, bottomrightY)
-        testinput = None
-    
-    countdown(3)
-    print("\nRunning...")
-    botStart()
-
-#start
-type = input("Type 1 if you want standard layout and type 2 if you want custom button placement > ")
-print("Stopping the program with holding 'esc'")
-#continuing the program
-if (type == '1'):
-    goStandard()
-elif(type == '2'):
-    goCustom()
-else:
-    print("Incorrect value, please input 1 or 2")
+start()
